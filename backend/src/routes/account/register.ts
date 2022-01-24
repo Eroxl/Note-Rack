776c66c4521b9f4750bfe693480b1af075d4df19
Eroxl <InfinityWaffles@outlook.com>
2022-01-23 21:14:56 -0800

    Added Error For Insufficient Register Params

--- backend/src/routes/account/register.ts
+++ backend/src/routes/account/register.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
 
 const router = express.Router();
 
@@ -6,6 +7,15 @@ router.post(
   '/register',
   async (req: Request, res: Response) => {
     const { username, email, password } = req.query;
+
+    // -=- Check that all parameters are supplied -=-
+    if (!(password && username && email)) {
+      res.status(400);
+      res.jsonp({
+        status: 'error',
+        reason: 'Insufficient parameters supplied to register an account!',
+      });
+    }
 
     res.jsonp({});
   },
