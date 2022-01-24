8c8841474677a209fadfd668b3d8e556723bf986
Eroxl <InfinityWaffles@outlook.com>
2022-01-23 20:50:08 -0800

    Updated User Model

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -2,7 +2,17 @@ import mongoose, { Schema } from 'mongoose';
 
 const UserScheme = new Schema({
   _id: Schema.Types.ObjectId,
-  username: Schema.Types.String,
+  username: {
+    type: Schema.Types.String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: Schema.Types.String,
+    required: true,
+    unique: true,
+  },
+  password: Schema.Types.String,
 });
 
 const UserModel = mongoose.models.user || mongoose.model('user', UserScheme, 'User');
