ce0d9242f55cecbea434b1abf37f896f3bb6a8ad
Eroxl <InfinityWaffles@outlook.com>
2022-01-24 20:58:46 -0800

    Main outline of page structure

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -4,8 +4,10 @@ import type { AppProps } from 'next/app';
 
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <div className="h-screen w-screen overflow-hidden">
+    <Component {...pageProps} />
+  </div>
+);
 
 export default MyApp;
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,6 +1,14 @@
 import type { NextPage } from 'next';
 import React from 'react';
 
-const Home: NextPage = () => <div />;
+import NavBar from '../components/home/NavBar';
+
+const Home: NextPage = () => (
+  <div className="h-screen w-screen bg-amber-50">
+    <div className="max-w-7xl h-full mx-auto">
+      <NavBar />
+    </div>
+  </div>
+);
 
 export default Home;
