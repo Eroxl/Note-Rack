ee960423f775aa83c61795be3985b8d7afc744fa
Eroxl <InfinityWaffles@outlook.com>
2022-01-26 10:11:24 -0800

    Remove redundant routes

--- backend/src/routes/account/index.ts
+++ backend/src/routes/account/index.ts
@@ -1,28 +1,14 @@
 import express from 'express';
 
-import refreshToken from './refreshToken';
 import register from './register';
-import logout from './logout';
 import login from './login';
 
 const router = express.Router();
-
-// -=- Create Refresh Token API -=-
-router.use(
-  '/',
-  refreshToken,
-);
 
 // -=- Create Register API -=-
 router.use(
   '/',
   register,
-);
-
-// -=- Create Logout API -=-
-router.use(
-  '/',
-  logout,
 );
 
 // -=- Create Login API -=-
