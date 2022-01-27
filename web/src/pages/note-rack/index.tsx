f94798403c12e4ae06e55cec2bd3229457a285c7
Eroxl <InfinityWaffles@outlook.com>
2022-01-26 16:53:58 -0800

    Fixed CSS to only work within the md editor

--- web/src/pages/note-rack/index.tsx
+++ web/src/pages/note-rack/index.tsx
@@ -7,7 +7,7 @@ const NoteRack = () => (
     <div className="pl-52 h-full w-full overflow-scroll mt-10 no-scrollbar">
       <div className="h-max w-full bg-amber-50 flex flex-col items-center">
         <div className="bg-blue-300 h-72 w-full -mb-10" />
-        <div className="max-w-4xl w-full text-zinc-700 break-words h-max px-20 flex flex-col gap-3 pb-24" />
+        <div className="max-w-4xl w-full text-zinc-700 break-words h-max px-20 flex flex-col gap-3 pb-24 editor" />
       </div>
     </div>
   </div>
