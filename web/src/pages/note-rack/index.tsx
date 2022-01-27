2642d618940ec6de8075ae01f030cd6ddd0f0352
Eroxl <InfinityWaffles@outlook.com>
2022-01-27 14:16:38 -0800

    Correctly redirect the page when the user signs in / when the access their homepage

--- web/src/pages/note-rack/index.tsx
+++ web/src/pages/note-rack/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import Router from 'next/router';
 
-const NoteRack = () => (
-  <div className="h-screen w-screen overflow-hidden bg-amber-50">
-    <div className="absolute w-screen h-10 bg-amber-50 z-10" />
-    <div className="absolute h-screen w-52 bg-amber-400 opacity-10" />
-    <div className="pl-52 h-full w-full overflow-scroll mt-10 no-scrollbar">
-      <div className="h-max w-full bg-amber-50 flex flex-col items-center">
-        <div className="bg-blue-300 h-72 w-full -mb-10" />
-        <div className="max-w-4xl w-full text-zinc-700 break-words h-max px-20 flex flex-col gap-3 pb-24 editor" />
-      </div>
-    </div>
-  </div>
-);
+const NoteRack = () => {
+  (async () => {
+    const pageID = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/page/get-home-page`, { method: 'POST', credentials: 'include' });
+    const pageJSON = await pageID.json();
+
+    if (pageJSON.status !== 'error') {
+      Router.push(`./note-rack/${pageJSON.message}`);
+    } else {
+      Router.push('./login');
+    }
+  })();
+
+  return <div />;
+};
 
 export default NoteRack;
