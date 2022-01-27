ec26460547061ef8156dd08b682a769d443cfcbf
Eroxl <InfinityWaffles@outlook.com>
2022-01-27 14:33:15 -0800

    Added a spinner component for loading screens

---
+++ web/src/components/Spinner.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Spinner = () => (
+  <div className="rounded-full border-solid animate-spin h-10 w-10 border-4 border-zinc-700 border-l-zinc-300 border-t-zinc-300" />
+);
+
+export default Spinner;
