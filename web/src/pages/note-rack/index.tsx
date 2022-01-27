9b89063943d064e122eebbad236f916656f72c22
Eroxl <InfinityWaffles@outlook.com>
2022-01-26 16:25:09 -0800

    Removed accidentally deleted part

--- web/src/pages/note-rack/index.tsx
+++ web/src/pages/note-rack/index.tsx
@@ -6,6 +6,8 @@ const NoteRack = () => (
     <div className="absolute h-screen w-52 bg-amber-400 opacity-10" />
     <div className="pl-52 h-full w-full overflow-scroll mt-10 no-scrollbar">
       <div className="h-max w-full bg-amber-50 flex flex-col items-center">
+        <div className="bg-blue-300 h-72 w-full -mb-10" />
+        <div className="max-w-4xl w-full text-zinc-700 break-words h-max px-20 flex flex-col gap-3 pb-24" />
       </div>
     </div>
   </div>
