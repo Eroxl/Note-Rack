3ee899593d9ffaff681b53f1e5607fd5d3d092b4
Eroxl <InfinityWaffles@outlook.com>
2022-01-27 14:01:55 -0800

    Added routes to get the page data if the user is authentication

---
+++ backend/src/routes/page/getPage.ts
@@ -0,0 +1,50 @@
+import express, { Request, Response } from 'express';
+
+import PageModel from '../../models/userModel';
+
+const router = express.Router();
+
+router.get(
+  '/get-page/:page',
+  async (req: Request, res: Response) => {
+    const { page } = req.query;
+    const { username } = res.locals;
+
+    if (!username) {
+      res.statusCode = 401;
+      res.json({
+        status: 'error',
+        message: 'Please login to view this page!',
+      });
+      return;
+    }
+
+    const pageData = await PageModel.findById(page).lean();
+
+    if (!pageData) {
+      res.statusCode = 404;
+      res.json({
+        status: 'error',
+        message: 'Page not found please try another page...',
+      });
+      return;
+    }
+
+    if (pageData.user !== username) {
+      res.statusCode = 403;
+      res.json({
+        status: 'error',
+        message: 'You do not have access to this file please login with a different account to view it...',
+      });
+      return;
+    }
+
+    res.statusCode = 200;
+    res.json({
+      status: 'success',
+      message: pageData,
+    });
+  },
+);
+
+export default router;
