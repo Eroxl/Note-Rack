0ff44518e4aca2480d0eaf5bd370791e1ba8ac86
Eroxl <InfinityWaffles@outlook.com>
2022-01-28 13:38:14 -0800

    Updated emoji when it's selected

--- web/src/components/page/Icon.tsx
+++ web/src/components/page/Icon.tsx
@@ -3,9 +3,16 @@ import 'emoji-mart/css/emoji-mart.css';
 import { Picker } from 'emoji-mart';
 
 const Icon = (props: { icon: string }) => {
-  const [isEmojiSelectorActive, setIsEmojiSelectorActive] = useState(false);
   const { icon } = props;
+  const [isEmojiSelectorActive, setIsEmojiSelectorActive] = useState(false);
+  const [currentIcon, setCurrentIcon] = useState(icon);
+
   const emojiPickerMenuRef = useRef<HTMLDivElement>(null);
+
+  const onEmojiChange = (emoji: any) => {
+    setCurrentIcon(emoji.native);
+    setIsEmojiSelectorActive(false);
+  };
 
   useEffect(() => {
     function handleClickOutside(event: any) {
@@ -21,17 +28,16 @@ const Icon = (props: { icon: string }) => {
   }, [emojiPickerMenuRef]);
 
   return (
-    <span
-      className="text-7xl select-none w-min relative"
-      ref={emojiPickerMenuRef}
-    >
-      <button type="button" onClick={() => { setIsEmojiSelectorActive(!isEmojiSelectorActive); }}>
-        {icon}
-      </button>
+    <div className="relative w-min h-min" ref={emojiPickerMenuRef}>
+      <span className="select-none w-20 h-20 hover:bg-slate-600 hover:bg-opacity-5 flex justify-center items-center">
+        <button className="text-7xl select-none w-min h-min" type="button" onClick={() => { setIsEmojiSelectorActive(!isEmojiSelectorActive); }}>
+          {currentIcon}
+        </button>
+      </span>
       <div className={`absolute sm:right-[-133px] right-[0%] top-[110%] ${isEmojiSelectorActive || 'hidden'}`}>
-        <Picker />
+        <Picker onSelect={onEmojiChange} />
       </div>
-    </span>
+    </div>
   );
 };
 
