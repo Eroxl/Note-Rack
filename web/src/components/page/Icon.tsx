281007a0fcd53f9aef7f184050ac065fb1490c7a
Eroxl <InfinityWaffles@outlook.com>
2022-01-28 13:08:07 -0800

    Added an emoji picker for changing the icon of a page

--- web/src/components/page/Icon.tsx
+++ web/src/components/page/Icon.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import 'emoji-mart/css/emoji-mart.css';
+import { Picker } from 'emoji-mart';
 
 const Icon = (props: { icon: string }) => {
   const { icon } = props;
 
-  return (<span role="img" className="text-7xl select-none">{icon}</span>);
+  return (
+    <span role="img" className="text-7xl select-none w-min relative">
+      {icon}
+      <div className="sm:flex hidden">
+        <Picker style={{ position: 'absolute', right: '-133px', top: '110%' }} />
+      </div>
+      <div className="flex sm:hidden">
+        <Picker style={{ position: 'absolute', right: '0%', top: '110%' }} />
+      </div>
+    </span>
+  );
 };
 
 export default Icon;
