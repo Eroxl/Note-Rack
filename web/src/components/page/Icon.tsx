d67e5aa3e062bbba439c2219f045fc6131717424
Eroxl <InfinityWaffles@outlook.com>
2022-01-28 13:10:00 -0800

    Cleaned up css to work with tailwind on the emoji picker

--- web/src/components/page/Icon.tsx
+++ web/src/components/page/Icon.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'emoji-mart/css/emoji-mart.css';
 import { Picker } from 'emoji-mart';
 
 const Icon = (props: { icon: string }) => {
+  const [isEmojiSelectorActive, setIsEmojiSelectorActive] = useState(false);
   const { icon } = props;
 
   return (
     <span role="img" className="text-7xl select-none w-min relative">
       {icon}
-      <div className="sm:flex hidden">
-        <Picker style={{ position: 'absolute', right: '-133px', top: '110%' }} />
-      </div>
-      <div className="flex sm:hidden">
-        <Picker style={{ position: 'absolute', right: '0%', top: '110%' }} />
+      <div className="absolute sm:right-[-133px] right-[0%] top-[110%]">
+        <Picker />
       </div>
     </span>
   );
