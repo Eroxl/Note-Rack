0481553707ce62c34098eb3f25d44168ae015fce
Eroxl <InfinityWaffles@outlook.com>
2022-01-28 16:32:17 -0800

    Updated page model to make querying for updates easier

--- backend/src/models/pageModel.ts
+++ backend/src/models/pageModel.ts
@@ -1,10 +1,15 @@
 import mongoose, { Schema } from 'mongoose';
+import crypto from 'crypto';
 
 const PageSchema = new Schema({
   user: String,
   data: [
     {
       blockType: String,
+      blockID: {
+        type: String,
+        default: () => crypto.randomBytes(20).toString('hex'),
+      },
       properties: {},
       style: {},
     },
