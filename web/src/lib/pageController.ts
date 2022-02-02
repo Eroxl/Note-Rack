135469128c6d5d635b1bc42f2ac75b4ffce0c65a
Eroxl <InfinityWaffles@outlook.com>
2022-02-02 00:25:06 -0800

    Moved page controls to separate script
    Moved all of the page controller related scripts like adding / deleting and modifying blocks into a separate script.

---
+++ web/src/lib/pageController.ts
@@ -0,0 +1,77 @@
+const updateServer = async (
+  blockID: string,
+  blockType: string | undefined,
+  properties: any | undefined,
+  style: any | undefined,
+  page: string,
+) => {
+  const updateResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/page/update-page/${page}`, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
+    body: JSON.stringify({
+      action: 'edit',
+      actionData: {
+        blockID,
+        data: {
+          ...(blockType && { blockType }),
+          ...(properties && { properties }),
+          ...(style && { style }),
+        },
+      },
+    }),
+  });
+
+  const updateResponseJSON = await updateResponse.json();
+
+  if (updateResponse.status === 200) return;
+
+  throw new Error(`Addition to server failed because of: ${updateResponseJSON.message}`);
+};
+
+const addToServer = async (blockType: string, index: number, page: string): Promise<number> => {
+  const addResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/page/update-page/${page}`, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
+    body: JSON.stringify({
+      action: 'add',
+      actionData: {
+        data: {
+          blockType,
+          index,
+        },
+      },
+    }),
+  });
+
+  const addResponseJSON = await addResponse.json();
+
+  if (addResponse.status === 200) return (addResponseJSON.message);
+
+  throw new Error(`Addition to server failed because of: ${addResponseJSON.message}`);
+};
+
+const removeFromServer = async (blockID: number, page: string) => {
+  const deleteResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/page/update-page/${page}`, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
+    body: JSON.stringify({
+      action: 'delete',
+      actionData: {
+        data: {
+          blockID,
+        },
+      },
+    }),
+  });
+
+  const deleteResponseJSON = await deleteResponse.json();
+
+  if (deleteResponse.status === 200) return;
+
+  throw new Error(`Deletion to server failed because of: ${deleteResponseJSON.message}`);
+};
+
+export { updateServer, addToServer, removeFromServer };
