4ee9697b136c58a3e170d18791b535c8efbb128f
Eroxl <InfinityWaffles@outlook.com>
2022-03-02 13:56:37 -0800

    Moved body components into the correct file

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -5,12 +5,10 @@ import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import '../styles/emojiPicker.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <body className="dark">
-    <div className="w-screen h-screen overflow-clip">
-      <Component {...pageProps} />
-    </div>
-  </body>
+const App = ({ Component, pageProps }: AppProps) => (
+  <div className="w-screen h-screen overflow-clip">
+    <Component {...pageProps} />
+  </div>
 );
 
-export default MyApp;
+export default App;
---
+++ web/src/pages/_document.tsx
@@ -0,0 +1,19 @@
+import {
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
+import React from 'react';
+
+const Document = () => (
+  <Html>
+    <Head />
+    <body className="dark">
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
