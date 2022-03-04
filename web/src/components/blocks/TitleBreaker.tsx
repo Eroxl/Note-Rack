f5d7eaa5ed47e5dc50ebff30dc588bb4a5f8a3d2
Eroxl <InfinityWaffles@outlook.com>
2022-03-04 14:01:23 -0800

    Fixed title breaker colour on dark mode

--- web/src/components/blocks/TitleBreaker.tsx
+++ web/src/components/blocks/TitleBreaker.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 const TitleBreaker = () => (
   <div className="
     w-full h-0.5 bg-black opacity-5 rounded-md
+    dark:bg-white
     print:bg-opacity-0
     print:relative print:overflow-hidden
     print:before:border-[999px]
