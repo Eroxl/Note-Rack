54251451090dcc13d3ce47b7b228b13f8c1281dd
Eroxl <InfinityWaffles@outlook.com>
2022-03-09 14:47:24 -0800

    Rewrote delete block route

---
+++ backend/src/routes/page/modify/deleteBlock.ts
@@ -0,0 +1,91 @@
+import mongoose from 'mongoose';
+import express, { Request, Response } from 'express';
+
+import PageModel from '../../../models/pageModel';
+
+const router = express.Router();
+
+router.patch(
+  '/delete-block',
+  async (req: Request, res: Response) => {
+    const { username } = res.locals;
+    const {
+      page,
+      docIDs,
+    } = req.body;
+
+    if (!username) {
+      res.statusCode = 401;
+      res.json({
+        status: 'error',
+        message: 'Please login to view this page!',
+      });
+      return;
+    }
+
+    const pageData = await PageModel.findOne({ _id: page, user: username }, 'user').lean();
+
+    if (!pageData) {
+      res.statusCode = 404;
+      res.json({
+        status: 'error',
+        message: 'You do not have access to this page or it does not exist...',
+      });
+      return;
+    }
+
+    if (!docIDs) {
+      res.statusCode = 400;
+      res.json({
+        status: 'error',
+        message: {
+          statusMessage: 'Please provide a list of docIDs in the body to delete an element...',
+        },
+      });
+      return;
+    }
+
+    const arrayFilters: Record<string, unknown>[] = [];
+    let queryString = 'data.';
+
+    (docIDs as string[]).forEach((element, index) => {
+      arrayFilters.push({
+        [`${index}._id`]: element,
+      });
+
+      if (index < (docIDs.length - 1)) {
+        queryString += `$[${index}].children.`;
+        return;
+      }
+
+      queryString += `$[${index}]`;
+    });
+
+    const newBlockId = new mongoose.Types.ObjectId();
+
+    await PageModel.updateOne(
+      {
+        _id: page,
+      },
+      {
+        $pull: {
+          [queryString]: {},
+        },
+      },
+      {
+        arrayFilters,
+      },
+    );
+
+    res.statusCode = 200;
+    res.json({
+      status: 'success',
+      message: {
+        statusMessage: 'Succesfully deleted block',
+        blockID: newBlockId,
+      },
+    });
+  },
+);
+
+export default router;
