2553fbaf2f8e4626eed2c663df5beb692ecee4b1
Eroxl <InfinityWaffles@outlook.com>
2022-04-05 21:28:57 -0700

    Allow blocks to be dragged to the top of the page

--- web/src/components/blocks/Title.tsx
+++ web/src/components/blocks/Title.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-to-interactive-role */
 import React from 'react';
+import { useDrop } from 'react-dnd';
 
 import TitleBreaker from './TitleBreaker';
 import { addBlockAtIndex } from '../../lib/pages/updatePage';
@@ -23,13 +24,29 @@ const Title = (props: TitleProps) => {
     editStyle({ name: text }, page);
   };
 
+  const [{ hovered }, drop] = useDrop(() => ({
+    accept: 'draggableBlock',
+    collect: (monitor) => ({
+      hovered: monitor.isOver() && (monitor.getItem() as { index: number }).index !== index,
+    }),
+    drop: (item) => {
+      const { index: itemIndex } = item as { index: number };
+      const pageDataCopy = { ...pageData };
+
+      pageDataCopy.message.data.splice(index, 0, pageData.message.data[itemIndex]);
+      pageDataCopy.message.data.splice(itemIndex + 1, 1);
+      setPageData(pageDataCopy);
+    },
+  }), [index]);
+
   return (
-    <>
+    <div className="relative flex flex-col">
       <span
-        className="text-5xl font-bold outline-none empty:before:content-['Untitled'] empty:before:opacity-30"
+        className="text-5xl font-bold outline-none empty:before:content-['Untitled'] empty:before:opacity-30 pb-3"
         contentEditable
         role="textbox"
         tabIndex={0}
+        ref={drop}
         onBlur={
           (e) => {
             onTitleChanged(e.currentTarget.innerText);
@@ -54,7 +71,8 @@ const Title = (props: TitleProps) => {
         {title}
       </span>
       <TitleBreaker />
-    </>
+      { hovered && <div className="absolute w-full h-0.5 bg-blue-400 -bottom-2 print:hidden" /> }
+    </div>
   );
 };
 
