c32a233ceb3b973d077a4f647bcef0fb41b1d3a9
Eroxl <InfinityWaffles@outlook.com>
2022-05-04 15:41:25 -0700

    📊 Setup docz

---
+++ packages/inline-math-input/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'import/order': [
+      'error',
+      {
+        groups: [['external', 'builtin'], 'internal', ['parent', 'sibling', 'index']],
+        'newlines-between': 'always',
+      },
+    ],
+    'import/extensions': 0,
+    'react/function-component-definition': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
---
+++ packages/inline-math-input/src/components/MathInput.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface MathInputProps {
+  defaultValue?: string;
+}
+
+const MathInput = (props: MathInputProps) => <div />;
+
+export default MathInput;
