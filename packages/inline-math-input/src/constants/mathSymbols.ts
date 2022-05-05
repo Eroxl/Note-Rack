540ed199ada88fef9cb0938b04f0268e199f9dc4
Eroxl <InfinityWaffles@outlook.com>
2022-05-04 23:15:26 -0700

    🖋 Added more math symbols

--- packages/inline-math-input/src/constants/mathSymbols.ts
+++ packages/inline-math-input/src/constants/mathSymbols.ts
@@ -31,6 +31,18 @@ const mathSymbols = [
     plaintext: '~~',
     unicode: '≈',
   },
+  {
+    plaintext: '~=',
+    unicode: '≈',
+  },
+  {
+    plaintext: 'inf',
+    unicode: '∞',
+  },
+  {
+    plaintext: 'infinity',
+    unicode: '∞',
+  },
 ];
 
 export default mathSymbols;
