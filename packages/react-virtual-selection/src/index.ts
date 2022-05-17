337fa76edd3758475516de4fa746d056282c209e
Eroxl <InfinityWaffles@outlook.com>
2022-05-17 15:04:46 -0700

    🔴 Setup react-virtual-selection package for npm

---
+++ packages/react-virtual-selection/src/index.ts
@@ -0,0 +1,9 @@
+// -=- Hooks -=-
+export * from './hooks/useSelectable';
+export * from './hooks/useSelectionCollector';
+
+// -=- Components -=-
+export * from './components/Selectable';
+
+// -=- Classes -=-
+export * from './classes/SelectionManager';
