f4e716f75063a14b3bf5d4bcf60571c02190c62b
Eroxl <InfinityWaffles@outlook.com>
2022-05-31 14:04:08 -0700

    Removed redundant console log

--- web/src/components/Editor.tsx
+++ web/src/components/Editor.tsx
@@ -35,8 +35,6 @@ const Editor = (
         for (let i = 0; i < selectionData.length; i += 1) {
           const { blockID, index } = selectionData[i] as { blockID: string, index: number };
 
-          console.log(index);
-
           // TODO:EROXL: 2020-05-21: Can't do more than one CRUD operation at a time.
           // eslint-disable-next-line no-await-in-loop
           await removeBlock(index - i, [blockID], page as string, pageData, setPageData);
