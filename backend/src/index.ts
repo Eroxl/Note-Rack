b211ea7146d260d34e6843ac03de420596457138
Eroxl <InfinityWaffles@outlook.com>
2022-07-16 22:55:09 -0700

    Setup docs for the backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
 import mongoose from 'mongoose';
 import express from 'express';
 import dotenv from 'dotenv';
@@ -28,6 +30,29 @@ app.use(verifyValidityOfToken);
 
 // -=- Add API Routes -=-
 app.use('/', routes);
+
+// -=- Setup Swagger -=-
+app.use(
+  '/docs',
+  swaggerUI.serve,
+  swaggerUI.setup(
+    swaggerJSDoc({
+      swaggerDefinition: {
+        info: {
+          title: 'REST API for my App',
+          version: '1.0.0',
+          description: 'This is the REST API for Note Rack',
+        },
+        host: '127.0.0.1:8000',
+        basePath: '/docs/',
+      },
+      apis: ['./documentation/**/*.yaml'],
+    }),
+    {
+      customSiteTitle: 'Note Rack REST API',
+    },
+  ),
+);
 
 // -=- Start The Express Server -=-
 app.listen(port);
