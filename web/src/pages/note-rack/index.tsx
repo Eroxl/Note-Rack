b61fde768c59c4ae08145c334f9a8a92ffdaa8a4
Eroxl <InfinityWaffles@outlook.com>
2022-12-30 17:34:30 -0800

    Only run home page fetcher client side
    Run the home page fetcher on the client side instead of being fetched at build time.

--- web/src/pages/note-rack/index.tsx
+++ web/src/pages/note-rack/index.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Router from 'next/router';
 
 const NoteRack = () => {
-  (async () => {
-    const pageID = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/page/get-home-page`, { method: 'POST', credentials: 'include' });
-    const pageJSON = await pageID.json();
+  // ~ Change this into useEffect
 
-    if (pageJSON.status !== 'error') {
-      Router.push(`./note-rack/${pageJSON.message}`);
-    } else {
-      Router.push('./login');
-    }
-  })();
+  useEffect(() => {
+    (async () => {
+      const pageID = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/page/get-home-page`, { method: 'POST', credentials: 'include' });
+      const pageJSON = await pageID.json();
+
+      if (pageJSON.status !== 'error') {
+        Router.push(`./note-rack/${pageJSON.message}`);
+      } else {
+        Router.push('./login');
+      }
+    })();
+  }, []);
 
   return <div />;
 };
