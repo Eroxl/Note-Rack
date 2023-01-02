f35c831c30775309875dc7baf555f383ebd755f3
Eroxl <InfinityWaffles@outlook.com>
2023-01-01 23:12:39 -0800

    Update home page redirect to use supertokens

--- web/src/pages/note-rack/index.tsx
+++ web/src/pages/note-rack/index.tsx
@@ -1,30 +1,45 @@
 /* eslint-disable no-empty */
 import React, { useEffect } from 'react';
 import Router from 'next/router';
+import Session from 'supertokens-auth-react/recipe/session';
 
 const NoteRack = () => {
   useEffect(() => {
     (async () => {
-      try {
-        const pageID = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/page/get-home-page`, { method: 'POST', credentials: 'include' });
+      // -=- Verification -=-
+      // ~ Check if the user is logged in
+      const session = await Session.doesSessionExist();
 
-        if (pageID.status === 401) {
-          Router.push('/auth/login');
-          return;
-        }
-        const pageJSON = await pageID.json();
+      // ~ If the user is not logged in, redirect them to the login page
+      if (session === false) {
+        Router.push('/auth');
+        return;
+      }
 
-        if (pageJSON.status !== 'error') {
-          Router.push(`./note-rack/${pageJSON.message}`);
-          return;
-        }
-      } catch (error) {}
+      // -=- Home Page Redirect -=-
+      // Get the user's home page
+      const pageID = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/page/get-home-page`,
+        {
+          method: 'POST',
+          credentials: 'include',
+        },
+      );
 
-      Router.push('/login');
+      const pageIDJSON = await pageID.json();
+
+      if (pageIDJSON.status !== 'error') {
+        Router.push(`./note-rack/${pageIDJSON.message}`);
+        return;
+      }
+
+      Router.push('/auth');
     })();
   }, []);
 
-  return <div />;
+  return (
+    <div className="relative flex flex-col items-center w-screen h-screen bg-amber-50 dark:bg-zinc-700" />
+  );
 };
 
 export default NoteRack;
