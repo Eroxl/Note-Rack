32a5c67741fbd7204565f6456133bf3c499be21f
Evan <infinitywaffles@outlook.com>
2023-01-08 16:52:25 -0800

    Delete setup_env.sh

--- ci/setup_env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp backend/.env.example backend/.env
-cp web/src/.env.example web/src/.env.local
-
-read -p "What is your text editor: " editor
-
-$editor backend/.env
-$editor web/src/.env.local
