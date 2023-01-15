e7ba8e3eb14ecf6eb21f6c1fdf47467e5de365e1
Eroxl <InfinityWaffles@outlook.com>
2023-01-14 22:30:41 -0800

    Only show relative paths

--- web/src/components/pageInfo/PagePath.tsx
+++ web/src/components/pageInfo/PagePath.tsx
@@ -25,31 +25,41 @@ const PagePath = () => {
   })
 
   return (
-    <div className="flex items-center w-full h-full gap-2 p-2 dark:text-amber-50 text-zinc-800 print:hidden">
-      {pagePath.map((page, index) => (
-        <Link
-          href={
-            {
-              pathname: '/note-rack/[page]',
-              query: { page: page.pageID },
+    <div className="flex items-center w-full gap-2 p-2 h-min dark:text-amber-50 text-zinc-800 print:hidden">
+      {pagePath.map((page, index) => {
+        if (index !== 0 && index < pagePath.length - 4) return null;
+
+        if (index !== 0 && index === pagePath.length - 4) return (
+          <span key={`page-path-${page.pageID}-ellipsis`} className="flex items-center gap-2">
+            <span>
+              ...
+            </span>
+            <span>
+              /
+            </span>
+          </span>
+        );
+
+        return (
+          <Link
+            href={
+              {
+                pathname: '/note-rack/[page]',
+                query: { page: page.pageID },
+              }
             }
-          }
-          prefetch={index !== pagePath.length - 1}
-        >
-          <a 
-            className="flex items-center gap-2"
-            href={`/note-rack/${page.pageID}`}
+            prefetch={index !== pagePath.length - 1}
           >
-            <span key={`page-path-${page.pageID}-icon`}>
-              {page.icon}
-            </span>
-            <span key={`page-path-${page.pageID}-name`}>
-              {page.name}
-            </span>
-            {index !== pagePath.length - 1 && <span key={`page-path-${page.pageID}-slash`}> / </span>}
-          </a>
-        </Link>
-      ))}
+            <a 
+              href={`/note-rack/${page.pageID}`}
+            >
+              {page.icon}{' '}
+              {page.name}{' '}
+              {index !== pagePath.length - 1 &&  "/"}
+            </a>
+          </Link>
+        )
+      })}
     </div>
   );
 };
