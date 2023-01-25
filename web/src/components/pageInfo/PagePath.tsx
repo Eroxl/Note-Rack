007df4b77dc8d27b6342a0948d98463b2d87bab0
Eroxl <InfinityWaffles@outlook.com>
2023-01-24 18:37:22 -0800

    Comment components

--- web/src/components/pageInfo/PagePath.tsx
+++ web/src/components/pageInfo/PagePath.tsx
@@ -11,14 +11,18 @@ const PagePath = () => {
   const [pagePath, setPagePath] = useState<PagePath[]>([]);
 
   useEffect(() => {
+    // ~ Handle the page path event
     const handlePagePath = (event: CustomEvent<PagePath[]>) => {
       const { detail } = event;
 
+      // ~ Set the page path state to re-render the component
       setPagePath(detail);
     };
 
+    // ~ Add the event listener
     document.addEventListener('pagePath', handlePagePath as EventListener);
 
+    // ~ Remove the event listener, when the component unmounts
     return () => {
       document.removeEventListener('pagePath', handlePagePath as EventListener);
     };
@@ -27,8 +31,10 @@ const PagePath = () => {
   return (
     <div className="flex items-center w-full gap-2 p-2 h-min dark:text-amber-50 text-zinc-800 print:hidden">
       {pagePath.map((page, index) => {
+        // ~ If the page is not the first page in the path, or the last 3 pages in the path, return null
         if (index !== 0 && index < pagePath.length - 4) return null;
 
+        // ~ If the page path is longer than 4 pages, return an ellipsis
         if (index !== 0 && index === pagePath.length - 4) return (
           <span key={`page-path-${page.pageID}-ellipsis`} className="flex items-center gap-2">
             <span>
@@ -40,6 +46,7 @@ const PagePath = () => {
           </span>
         );
 
+        // ~ Return the page path link
         return (
           <Link
             href={
