073b1c10777f478c38cd1fd0605ea08f4bb6a9e5
Eroxl <InfinityWaffles@outlook.com>
2023-04-01 18:38:57 -0700

    Simplify search modal styling

--- web/src/components/modals/SearchModal.tsx
+++ web/src/components/modals/SearchModal.tsx
@@ -27,19 +27,19 @@ const SearchModal = () => {
       setIsOpen={setIsOpen}
       className="top-24 -translate-y-[0%]"
     >
-      <div
-        className={`
-          flex flex-col justify-center w-screen h-16 max-w-2xl
-          p-1 border-[20px] rounded-lg border-zinc-700
-          before:rounded before:top-2 before:left-2
-          before:bottom-2 before:right-2 before:absolute before:bg-zinc-600
-        `}
-      >
-        <input
-          className="z-20 text-xl text-white bg-transparent border-0 outline-none"
-          placeholder='Search notes...'
-          ref={inputRef}
-        /> 
+      <div className="flex flex-col w-screen max-w-2xl gap-2 p-2 rounded-lg bg-zinc-700">
+        <div
+          className={`
+            flex flex-col justify-center h-12 max-w-2xl
+            p-2 rounded bg-zinc-600
+          `}
+        >
+          <input
+            className="text-xl text-white bg-transparent border-0 outline-none"
+            placeholder='Search notes...'
+            ref={inputRef}
+          /> 
+        </div>
       </div>
     </BaseModal>
   );
