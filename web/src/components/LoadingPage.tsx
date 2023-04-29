ae05820df6326d51b7e27c21db07142c786acd8e
Eroxl <InfinityWaffles@outlook.com>
2023-04-29 15:16:46 -0700

    Fix loading spacing when chat panel is active

--- web/src/components/LoadingPage.tsx
+++ web/src/components/LoadingPage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Spinner from './Spinner';
 
 const LoadingPage = () => (
-  <div className="w-full h-full mt-10 overflow-hidden pl-52">
+  <div className="w-full h-full mt-10 overflow-hidden">
     <div className="flex flex-col items-center w-full h-screen bg-amber-50 dark:bg-zinc-700">
       <div className="flex flex-col items-center justify-center w-full h-full max-w-4xl px-20 break-words pb-36 text-zinc-700 dark:text-amber-50">
         <Spinner />
