2bb03ef2b58133a658c8843b0935517d0d547867
Eroxl <InfinityWaffles@outlook.com>
2023-04-29 19:03:34 -0700

    Add button to clear chat

--- web/src/components/Chat.tsx
+++ web/src/components/Chat.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState, useRef } from 'react';
+import Image from 'next/image';
 import { useSessionContext } from 'supertokens-auth-react/recipe/session'; 
 
-import type { PagePath } from './pageInfo/PagePath';
+import Trash from '../public/icons/Trash.svg';
+import Brain from '../public/icons/Brain.svg';
 
 interface ChatMessage {
   role: 'system' | 'user' | 'assistant',
@@ -10,10 +12,10 @@ interface ChatMessage {
 
 const Chat = () => {
   const [chatMessages, setChatMessages] = useState<ChatMessage[]>([]);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const chatRef = useRef<HTMLDivElement>(null);
 
   const session = useSessionContext();
-  const isLoggedIn = session?.loading === false && session?.doesSessionExist === true;
 
   // ~ Ask the question to the bot
   const askQuestion = async (chatMessages: ChatMessage[], newMessage: string): Promise<ChatMessage[]> => {
@@ -48,10 +50,74 @@ const Chat = () => {
   return (
     <div className="w-full h-full mt-10 overflow-y-auto overflow-x-clip">
       <div className="relative w-full h-screen max-w-4xl mx-auto">
+        {
+          isDeleteModalOpen && (
+            <div
+              className="absolute z-10 flex flex-col items-center justify-center w-full h-full bg-black/30"
+              onClick={() => {
+                setIsDeleteModalOpen(false);
+              }}
+            >
+              <div
+                className="absolute flex flex-col items-center justify-center p-8 -translate-y-2/3 bg-zinc-700 rounded-xl top-1/2"
+                onClick={(e) => {
+                  e.stopPropagation();
+                }}
+              >
+                <p className="text-2xl font-bold text-center text-amber-50">
+                  Delete chat history
+                </p>
+                <p className="text-lg text-center text-amber-50">
+                  Are you sure you want to delete
+                  <br />
+                  your chat history?
+                </p>
+                <div className="flex flex-row w-full gap-2 mt-5">
+                  <button
+                    className="flex items-center justify-center w-full p-2 text-xl font-bold bg-blue-400 rounded-md text-amber-50"
+                    onClick={() => setIsDeleteModalOpen(false)}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    className="flex items-center justify-center w-full p-2 text-xl font-bold bg-red-400 rounded-md text-amber-50"
+                    onClick={() => {
+                      setIsDeleteModalOpen(false);
+                      localStorage.removeItem('chatMessages');
+                      setChatMessages([]);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </div>
+          )
+        }
         <div
           className="flex flex-col w-full h-[calc(100%-8rem)] mx-auto overflow-scroll no-scrollbar px-20"
           ref={chatRef}
         >
+          {chatMessages.length === 0 && (
+            <div className="flex flex-col justify-center w-full h-full text-center">
+              <p className="flex items-center justify-center pb-3 text-2xl font-bold text-amber-50">
+                <Image
+                  src={Brain}
+                  alt="Brain"
+                  width={50}
+                  height={50}
+                />
+                Chat
+              </p>
+              <p className="text-xl font-medium text-amber-50">
+                Ask a question to get started
+              </p>
+              <p className="text-amber-50">
+                Note Rack chat allows you to ask questions about the page you are on,
+                and get answers from an interactive assistant.
+              </p>
+            </div>
+          )}
           {
             chatMessages.map((message, index) => (
               <div
@@ -76,9 +142,9 @@ const Chat = () => {
             ))
           }
         </div>
-        <div className="absolute w-full max-w-4xl px-20 mx-auto translate-x-1/2 -translate-y-full bottom-5 right-1/2">
+        <div className="absolute flex flex-row w-full max-w-4xl gap-2 px-20 mx-auto translate-x-1/2 -translate-y-full bottom-5 right-1/2">
           <input
-            className="relative w-full p-2 overflow-scroll text-white break-normal rounded max-h-12 bg-white/10 focus:outline-none"
+            className="relative flex-grow p-2 px-3 overflow-scroll text-white break-normal rounded-md max-h-12 bg-white/10 focus:outline-none"
             placeholder="Ask a question..."
             type="text"
             onKeyDown={(e) => {
@@ -110,6 +176,17 @@ const Chat = () => {
               })();
             }}
           />
+          <button
+            className="flex items-center justify-center w-12 h-12 bg-red-400 rounded-md"
+            onClick={() => setIsDeleteModalOpen(true)}
+          >
+            <Image
+              src={Trash}
+              alt="Trash"
+              width={26}
+              height={26}
+            />
+          </button>
         </div>
       </div>
     </div>
